1b79339889703cd9a94a6c4ffab32f3ac6bb48b2
Li Shengqiu <TuesdaysLinearA@gmail.com>
2012-08-14 17:47:46 +0800

    umeng sns

--- gen/org/footoo/cjflsq/neck/R.java
+++ gen/org/footoo/cjflsq/neck/R.java
@@ -8,6 +8,10 @@
 package org.footoo.cjflsq.neck;
 
 public final class R {
+    public static final class anim {
+        public static final int umeng_share_popup_enter=0x7f040000;
+        public static final int umeng_share_popup_exit=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -20,42 +24,130 @@ public final class R {
         public static final int guide_dot_white=0x7f020006;
         public static final int ic_action_search=0x7f020007;
         public static final int ic_launcher=0x7f020008;
+        public static final int umeng_share_bg_gridview=0x7f020009;
+        public static final int umeng_share_bg_shareitem_nor=0x7f02000a;
+        public static final int umeng_share_bg_shareitem_sel=0x7f02000b;
+        public static final int umeng_share_face_01=0x7f02000c;
+        public static final int umeng_share_face_02=0x7f02000d;
+        public static final int umeng_share_face_03=0x7f02000e;
+        public static final int umeng_share_face_04=0x7f02000f;
+        public static final int umeng_share_face_05=0x7f020010;
+        public static final int umeng_share_face_06=0x7f020011;
+        public static final int umeng_share_face_07=0x7f020012;
+        public static final int umeng_share_face_08=0x7f020013;
+        public static final int umeng_share_face_09=0x7f020014;
+        public static final int umeng_share_face_10=0x7f020015;
+        public static final int umeng_share_face_11=0x7f020016;
+        public static final int umeng_share_face_12=0x7f020017;
+        public static final int umeng_share_gridview_item=0x7f020018;
+        public static final int umeng_share_gridview_item_sel=0x7f020019;
+        public static final int umeng_share_logo_doub=0x7f02001a;
+        public static final int umeng_share_logo_renren=0x7f02001b;
+        public static final int umeng_share_logo_sina=0x7f02001c;
+        public static final int umeng_share_logo_tenc=0x7f02001d;
+        public static final int umeng_share_send=0x7f02001e;
+        public static final int umeng_share_send_button_nor=0x7f02001f;
+        public static final int umeng_share_send_button_sel=0x7f020020;
+        public static final int umeng_share_shareitem=0x7f020021;
+        public static final int umeng_share_shareto=0x7f020022;
+        public static final int umeng_share_shareto_stroke=0x7f020023;
+        public static final int umeng_share_shareto_title=0x7f020024;
+        public static final int umeng_share_update_bottom=0x7f020025;
+        public static final int umeng_share_update_button=0x7f020026;
+        public static final int umeng_share_update_button_nor=0x7f020027;
+        public static final int umeng_share_update_button_sel=0x7f020028;
+        public static final int umeng_share_update_main=0x7f020029;
     }
     public static final class id {
-        public static final int aboutText=0x7f070005;
-        public static final int anim=0x7f070001;
-        public static final int button1=0x7f070006;
-        public static final int infoText=0x7f070003;
-        public static final int linearLayout1=0x7f070002;
-        public static final int mPager=0x7f070000;
-        public static final int menu_settings=0x7f07000c;
-        public static final int settingText=0x7f070004;
-        public static final int startButton=0x7f070009;
-        public static final int textView1=0x7f070007;
-        public static final int tttttt=0x7f070008;
-        public static final int viewGroup=0x7f07000b;
-        public static final int viewPager=0x7f07000a;
+        public static final int aboutText=0x7f080005;
+        public static final int anim=0x7f080001;
+        public static final int background=0x7f080012;
+        public static final int bottom=0x7f080018;
+        public static final int btn=0x7f08000c;
+        public static final int btnFaces=0x7f08001d;
+        public static final int btnFrom=0x7f08001f;
+        public static final int btnSend=0x7f080020;
+        public static final int btnToRenR=0x7f080010;
+        public static final int btnToSina=0x7f08000e;
+        public static final int btnToTenc=0x7f08000b;
+        public static final int btnTopic=0x7f08001e;
+        public static final int button1=0x7f080006;
+        public static final int edit=0x7f080019;
+        public static final int gridView=0x7f08001c;
+        public static final int image=0x7f08001a;
+        public static final int infoText=0x7f080003;
+        public static final int linearLayout1=0x7f080002;
+        public static final int mPager=0x7f080000;
+        public static final int main=0x7f080017;
+        public static final int menu_settings=0x7f080024;
+        public static final int nickName=0x7f08001b;
+        public static final int settingText=0x7f080004;
+        public static final int shareto=0x7f080009;
+        public static final int shareto_top=0x7f08000a;
+        public static final int startButton=0x7f080021;
+        public static final int textView1=0x7f080007;
+        public static final int title=0x7f080014;
+        public static final int title_bar=0x7f080013;
+        public static final int topic_listview=0x7f080015;
+        public static final int tttttt=0x7f080008;
+        public static final int tvrenr=0x7f080011;
+        public static final int tvsina=0x7f08000f;
+        public static final int tvtenc=0x7f08000d;
+        public static final int viewGroup=0x7f080023;
+        public static final int viewPager=0x7f080022;
+        public static final int wordCount=0x7f080016;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int info=0x7f030001;
         public static final int settings=0x7f030002;
-        public static final int welcome_item1=0x7f030003;
-        public static final int welcome_item2=0x7f030004;
-        public static final int welcome_item3=0x7f030005;
-        public static final int welcome_item4=0x7f030006;
-        public static final int welcome_page=0x7f030007;
+        public static final int umeng_share_shareto=0x7f030003;
+        public static final int umeng_share_topics=0x7f030004;
+        public static final int umeng_share_update=0x7f030005;
+        public static final int welcome_item1=0x7f030006;
+        public static final int welcome_item2=0x7f030007;
+        public static final int welcome_item3=0x7f030008;
+        public static final int welcome_item4=0x7f030009;
+        public static final int welcome_page=0x7f03000a;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
+        public static final int umeng_share_oauth_linkserver=0x7f050016;
+        public static final int umeng_share_renr=0x7f05001b;
+        public static final int umeng_share_shareto_renr=0x7f050005;
+        public static final int umeng_share_shareto_sina=0x7f050006;
+        public static final int umeng_share_shareto_tenc=0x7f050007;
+        public static final int umeng_share_shareto_top=0x7f050004;
+        public static final int umeng_share_sina=0x7f05001c;
+        public static final int umeng_share_snsservice_linkServer=0x7f050017;
+        public static final int umeng_share_snsservice_noNetwork=0x7f05001a;
+        public static final int umeng_share_snsservice_oauthFailed=0x7f050019;
+        public static final int umeng_share_snsservice_oauthSuccessed=0x7f050018;
+        public static final int umeng_share_tenc=0x7f05001d;
+        public static final int umeng_share_update_appBlocked=0x7f050015;
+        public static final int umeng_share_update_contentEmpty=0x7f05000b;
+        public static final int umeng_share_update_edittext_hint=0x7f050008;
+        public static final int umeng_share_update_extendsSendLimit=0x7f050013;
+        public static final int umeng_share_update_fileNotExist=0x7f050010;
+        public static final int umeng_share_update_fileToLarge=0x7f05000f;
+        public static final int umeng_share_update_imgLoadFailed=0x7f05000a;
+        public static final int umeng_share_update_msgLengthExtendsLimit=0x7f050011;
+        public static final int umeng_share_update_networkUnavailable=0x7f050012;
+        public static final int umeng_share_update_repeated=0x7f05000e;
+        public static final int umeng_share_update_send=0x7f050009;
+        public static final int umeng_share_update_uidNotExist=0x7f050014;
+        public static final int umeng_share_update_uneditTopic=0x7f05000c;
+        public static final int umeng_share_update_updated=0x7f05000d;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+        public static final int InfoPopAnimation=0x7f060001;
+        public static final int Theme_IOSched=0x7f060002;
     }
 }
