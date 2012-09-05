6e55899351c5b1b5f4755da7479a04234bea72f4
Chingfan Tsou <caojingfan@gmail.com>
2012-09-05 21:58:40 +0800

    修复分支合并之后的bug

--- src/org/footoo/cjflsq/neck/cervicaltest/CervicalTestActivity.java
+++ src/org/footoo/cjflsq/neck/cervicaltest/CervicalTestActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.content.Intent;
 
 public class CervicalTestActivity extends FragmentActivity {
     private TestViewPager mPager;
