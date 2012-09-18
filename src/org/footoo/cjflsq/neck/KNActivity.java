3dec1db25ff59f773abfd844c7ab5e052594e32d
Chingfan Tsou <caojingfan@gmail.com>
2012-09-18 22:42:20 +0800

    备份

---
+++ src/org/footoo/cjflsq/neck/KNActivity.java
@@ -0,0 +1,12 @@
+package org.footoo.cjflsq.neck;
+  
+import android.app.Activity;  
+import android.os.Bundle;  
+
+public class KNActivity extends Activity {  
+
+    public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.knowledge_layout);  
+    }  
+}
